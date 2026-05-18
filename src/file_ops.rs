40c21e053fbcfeb1446f3c3ddd3da6cbbde8df9e
akshay.s <adiosphobian@gmail.com>
2026-05-18 14:55:54 +0530

    init file in worker exec

--- src/file_ops.rs
+++ src/file_ops.rs
@@ -3,31 +3,36 @@ use anyhow::{Result, Context};
 use crate::LogOutput;
 use std::io::{Seek, BufRead, SeekFrom};
 use std::fs::File;
+use std::sync::Arc;
 
 pub struct ChunkedWorker{
     start_offset: u64, 
     end_offset : u64,
-    reader: BufReader<File>,
+    path: Arc<std::path::PathBuf>,
 }
 
 impl ChunkedWorker{
-    fn new(path : &std::path::Path, start_offset : u64, end_offset: u64) -> Result<Self>{
-        let mut file = File::open(path)?;
-        file.seek(SeekFrom::Start(start_offset))?;
-        let reader = BufReader::with_capacity(1024 * 1024 * 100, file); 
+    fn new(path : Arc<std::path::PathBuf>, start_offset : u64, end_offset: u64) -> Result<Self>{
+        //let mut file = File::open(path)?;
+        //file.seek(SeekFrom::Start(start_offset))?;
+        //let reader = BufReader::with_capacity(1024 * 1024 * 100, file); 
         Ok(Self{
             start_offset,
             end_offset,
-            reader: reader,
+            path,
         })
     }
     
     pub fn analyze(mut self) -> Result<LogOutput>{
         let mut log_output = LogOutput::default();
+        let mut file = File::open(self.path.as_path())?;
+        file.seek(SeekFrom::Start(self.start_offset))?;
+        let mut reader = BufReader::with_capacity(1024* 1024 * 100, file);
+
         let mut buffer = Vec::new();
         while self.start_offset < self.end_offset {
             buffer.clear();
-            let bytes_read = self.reader.read_until(b'\n', &mut buffer)?;
+            let bytes_read = reader.read_until(b'\n', &mut buffer)?;
             if bytes_read ==0 {
                 break;
             }
@@ -41,15 +46,15 @@ impl ChunkedWorker{
 }
 
 
-pub fn chunk_file(path: &std::path::Path, chunks: usize) -> Result<Vec<ChunkedWorker>>{
-    let file_metadata = std::fs::metadata(path).context("metadata file read failed")?;
+pub fn chunk_file(path: &std::sync::Arc<std::path::PathBuf>, chunks: usize) -> Result<Vec<ChunkedWorker>>{
+    let file_metadata = std::fs::metadata(path.as_path()).context("metadata file read failed")?;
     let file_size = file_metadata.len();
     let mut workers = Vec::with_capacity(chunks);
     let chunk_size = file_size / (chunks as u64);
 
     let mut start_chunk = 0;
     let mut end_chunk;
-    let file = File::open(path)?;
+    let file = File::open(path.as_path())?;
     let mut reader = BufReader::new(file);
     let mut buffer = Vec::new();
 
@@ -62,9 +67,9 @@ pub fn chunk_file(path: &std::path::Path, chunks: usize) -> Result<Vec<ChunkedWo
         reader.seek(SeekFrom::Start(end_chunk))?;
         let bytes_read = reader.read_until(b'\n', &mut buffer)?;
         end_chunk += bytes_read as u64;
-        workers.push(ChunkedWorker::new(path, start_chunk, end_chunk)?);
+        workers.push(ChunkedWorker::new(Arc::clone(path), start_chunk, end_chunk)?);
         start_chunk = end_chunk +1;
     }
-    workers.push(ChunkedWorker::new(path, start_chunk, file_size)?);
+    workers.push(ChunkedWorker::new(Arc::clone(path), start_chunk, file_size)?);
     Ok(workers)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,69 @@
-use std::path::Path;
+use std::path::PathBuf;
 //use rayon::prelude::*;
 mod file_ops;
 
 fn main() -> Result<()>{
-    let res = run_analyzer(Path::new("test.dat"))?;
+    let res = run_analyzer(PathBuf::from("test.dat"))?;
     println!("{}", res);
     Ok(())
 }
 
-fn run_analyzer(path: &std::path::Path) -> Result<LogOutput>{
-    let num = std::thread::available_parallelism()?;
-    let mut workers = file_ops::chunk_file(path, num.get()).unwrap();
+fn run_analyzer(path: std::path::PathBuf) -> Result<LogOutput>{
+    //let num = std::thread::available_parallelism()?;
+    let path = std::sync::Arc::new(path);
+    let core_ids = core_affinity::get_core_ids().ok_or(anyhow::anyhow!("couldnt get core id"))?;
+    let num = core_ids.len() * 4;
+    let mut workers = file_ops::chunk_file(&path, num).unwrap();
     if workers.len() == 1{
         workers.pop().ok_or(anyhow::anyhow!("failed to get worker")).unwrap().analyze()
     }else{
         let mut result = Ok(LogOutput::default());
         let point = &mut result;
+
+        let (tx, rx) = crossbeam_channel::unbounded();
+        for worker in workers{
+            tx.send(worker)?;
+        }
+        drop(tx);
         std::thread::scope(move |scope| {
-            let mut results = Vec::with_capacity(workers.len());
-            for worker in workers{
+            let mut handles = Vec::with_capacity(core_ids.len());
+            for core_id in core_ids{
+                let rx = rx.clone();
                 let handle = scope.spawn(move || {
-                    worker.analyze()
+                    core_affinity::set_for_current(core_id);
+                    let mut local_log_output = LogOutput::default();
+
+                    while let Ok(worker) = rx.recv(){
+                        if let Ok(res)  = worker.analyze(){
+                            local_log_output += res;
+                        }
+                    }
+                    return local_log_output;
                 });
-                results.push(handle);
+                handles.push(handle);
             }
-            *point = Ok(results.into_iter()
+
+            *point = Ok(handles.into_iter()
                         .map(|handle| handle.join())
                         .filter_map(Result::ok)
-                        .filter_map(Result::ok)
-                        .sum::<LogOutput>());
-                
+                        .sum()
+            );
+           
+
+
+            //let mut results = Vec::with_capacity(workers.len());
+            //for worker in workers{
+            //    let handle = scope.spawn(move || {
+            //        worker.analyze()
+            //    });
+            //    results.push(handle);
+            //}
+            //*point = Ok(results.into_iter()
+            //            .map(|handle| handle.join())
+            //            .filter_map(Result::ok)
+            //            .filter_map(Result::ok)
+            //            .sum::<LogOutput>());
+            //    
         });
       result
         //Ok(workers
